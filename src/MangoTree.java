744bbded2e15aa188ff384892224359e6ad25f19
Nandhini S <89440905+nandhuscodes@users.noreply.github.com>
2023-08-15 16:45:36 +0530

    Update MangoTree.java

--- src/MangoTree.java
+++ src/MangoTree.java
@@ -4,7 +4,7 @@ public class MangoTree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int rows = scanner.nextInt(), cols = scanner.nextInt(), trees = scanner.nextInt();
-        if(trees <= cols || trees%cols == 1 || trees%cols==0){
+        if(trees <= rows || trees%rows == 1 || trees%rows==0){
             System.out.println("true");
         }else {
             System.out.println("false");
